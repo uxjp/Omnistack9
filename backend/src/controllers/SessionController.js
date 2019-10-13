4ceae678a0428102e1e77dba4fca80ff0d694e1c
johnpnx <joaopedro1172@gmail.com>
2019-10-13 18:05:13 -0300

    Working. MVC with V. \n User with session controler

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,20 @@
+// INDEX, SHOW, UPDATE, STORE, DESTROYS
+const User = require('../models/User');
+
+module.exports = {
+    async store( req, res) {
+        //return res.json({ message : "hello world session controller"});
+        const email = req.body.email;
+        //const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if(!user){
+            user = await User.create({ email });
+        }
+
+
+        return res.json(user);
+    }
+
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-
-    return res.send("Hello world");
-    
+mongoose.connect('mongodb+srv://johnpnx:86909196@omni-gpz7n.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
 });
+
+// GET POST PUT PATCH DELETE
+
+//usar formato padrao
+app.use(express.json())
+app.use(routes);
+
+
 
 app.listen(3333);
