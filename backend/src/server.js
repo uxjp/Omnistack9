cd487f0d1a808f3561c013083c3873fd7135098a
johnpnx <joaopedro1172@gmail.com>
2020-02-03 18:48:20 -0300

    Socketio + express + node http module. Mentioned Redis for fast storaged of unrelated data

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,39 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 
 const app = express();
+
+const server = http.Server(app); //separa o servidor http da minha instacia do express
+const io = socketio(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    console.log(socket.handshake.query);
+    console.log('usuario conectado', socket.id)
+
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id; 
+    /*  
+    // setTimeout(() => {
+    //     socket.emit('hello', 'world');
+        
+    // }, 4000); // mensagem partindo do servidor
+
+    socket.on('omni', (data) => {
+        console.log(data);
+    }) 
+  */
+    });
+
+// socket nao funciona com o insomnia
+// existem ferramnetas para o Browser
+
 
 mongoose.connect('mongodb+srv://johnpnx:86909196@omni-gpz7n.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser : true,
@@ -22,4 +53,4 @@ app.use(routes);
 
 
 
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
+import socketio from 'socket.io-client';
+
 
 import './styles.css';
 
@@ -9,7 +11,15 @@ import './styles.css';
 export default function Dashboard() {
     const [spots, setSpots] = useState([]); 
 
-    // estudar react hooks
+    useEffect( () => {
+        const user_id = localStorage.getItem('user');
+        const socket = socketio('http://localhost:3333', {
+            query : { user_id },
+        });
+
+        // socket.emit('omni', 'Stack');  // responde ao backend uma mensagem que tenha o nome 'omni'
+    }, [])
+    
     useEffect( () => {
         
         async function loadSpots() {
